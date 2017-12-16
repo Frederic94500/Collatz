2ab281c1ba2e709d6fb945b739c21e5daf4df481
Frédéric TRAN <frederictran@hotmail.fr>
2017-12-16 10:00:42 +0100

    Collatz début

--- Collatz/Collatz.py
+++ Collatz/Collatz.py
@@ -1,1 +1,13 @@
-
+print ("Entrer une valeur:")
+n = int(input())
+print (n)
+tdvol = 1
+while n!=1:
+    tdvol = tdvol + 1
+    if n%2==0:
+        n = n/2
+        print (n)
+    else:
+        n = 3*n+1
+        print (n)
+print ("Le temps de vol est égale à" , tdvol)
